585d558c234770cf28df27626956e1cdb9faeafe
yes-im-harsh <harshchauhan.cse279@gmail.com>
2021-09-16 20:41:39 +0530

    2. Mapping and Create Task Created in solidity

--- contracts/Todolist.sol
+++ contracts/Todolist.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract Todolist {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public Tasks;
+
+    constructor() public {
+        createTask("This is my first Dapp");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        Tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
